477781c0efdef2c0e00153f000c35c7165c3e99c
ivanurra <ivan.urra.wd@gmail.com>
2021-06-10 12:22:20 +0200

    directory

---
+++ 06-directorios/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Crear carpeta
+// if (!is_dir('new_folder')) {
+//     mkdir('new_folder', 0777) or die('No se puede crear la carpeta.');
+//     echo "Carpeta creada.";
+// } else {
+//     echo "Ya existe la carpeta.";
+// }
+
+// Borrar carpeta
+// rmdir("new_folder");
